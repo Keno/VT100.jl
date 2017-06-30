871bd862cf977108fcceac50db5e7b707ef4a718
Keno Fischer <kfischer@college.harvard.edu>
2017-06-30 17:56:34 -0400

    That's what you get for work on on case-insensitive file systems

--- test/TRTtest.jl
+++ test/TRTtest.jl
@@ -2,7 +2,7 @@ include("TerminalRegressionTests.jl")
 
 const thisdir = dirname(@__FILE__)
 TerminalRegressionTests.automated_test(
-                joinpath(thisdir,"trt.multiout"),
+                joinpath(thisdir,"TRT.multiout"),
                 ["Julia\n","Yes!!\n"]) do emuterm
     print(emuterm, "Please enter your name: ")
     name = strip(readline(emuterm))
